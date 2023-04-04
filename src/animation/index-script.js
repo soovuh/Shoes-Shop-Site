4d3a2af0087f1c3ffb56ec3e76c1fad24c535147
Klymenko Illia <sovuh2703@gmail.com>
2023-04-04 14:08:23 +0300

    Creating basic frame of the cart_page without js

--- src/animation/index-script.js
+++ src/animation/index-script.js
@@ -1,100 +1,75 @@
-const carousel = document.querySelector('.carousel');
-const arrowIcons = document.querySelectorAll('.carousel .icon');
+const carousel = document.querySelector(".carousel");
+const arrowIcons = document.querySelectorAll(".carousel .icon");
 
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+let isDragStart = false,
+  isDragging = false,
+  prevPageX,
+  prevScrollLeft,
+  positionDiff;
 let firstImgWidth;
 let scrollWidth;
 
-
-
-const showHideIcons = () => {
-    scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-    if (Math.ceil(carousel.scrollLeft == 0)) {
-        arrowIcons[0].style.display = 'none';
-
-    }
-    else {
-        arrowIcons[0].style.display = 'block';
-    }
-    if (Math.ceil(carousel.scrollLeft) == scrollWidth) {
-        arrowIcons[1].style.display = 'none';
-
-    }
-    else {
-        arrowIcons[1].style.display = 'block';
-    }
-}
-
 const widthCalculate = () => {
-    const firstImg = carousel.querySelector('.firstImg');
-    firstImgWidth = Math.ceil(firstImg.scrollWidth) + 10;
-    return firstImgWidth;
-}
+  const firstImg = carousel.querySelector(".firstImg");
+  firstImgWidth = Math.ceil(firstImg.scrollWidth) + 9;
+  return firstImgWidth;
+};
 
-
-arrowIcons.forEach(icon => {
-    firstImgWidth = widthCalculate();
-    icon.addEventListener('click', () => {
-        carousel.scrollLeft += icon.id == 'left' ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60);
-    })
+arrowIcons.forEach((icon) => {
+  firstImgWidth = widthCalculate();
+  icon.addEventListener("click", () => {
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60);
+  });
 });
 
 const autoSlide = () => {
-    if (carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
-    positionDiff = Math.abs(positionDiff);
-    firstImgWidth = widthCalculate();
-    let valDiff = firstImgWidth - positionDiff;
-    if (carousel.scrollLeft > prevScrollLeft) {
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDiff : -positionDiff;
-    }
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDiff : -positionDiff;
-}
+  if (carousel.scrollLeft == carousel.scrollWidth - carousel.clientWidth)
+    return;
+  positionDiff = Math.abs(positionDiff);
+  firstImgWidth = widthCalculate();
+  let valDiff = firstImgWidth - positionDiff;
+  if (carousel.scrollLeft > prevScrollLeft) {
+    return (carousel.scrollLeft +=
+      positionDiff > firstImgWidth / 3 ? valDiff : -positionDiff);
+  }
+  carousel.scrollLeft -=
+    positionDiff > firstImgWidth / 3 ? valDiff : -positionDiff;
+};
 
 const dragStart = (e) => {
-    // updating global variables value on touch start or mouse down event
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
+  // updating global variables value on touch start or mouse down event
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
 
 const dragging = (e) => {
-    // scrolling images/carousel to left according to touch or mouse pointer
-    if (!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    carousel.classList.add('dragging');
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
-}
+  // scrolling images/carousel to left according to touch or mouse pointer
+  if (!isDragStart) return;
+  e.preventDefault();
+  isDragging = true;
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+};
 
 const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove('dragging');
-    if (!isDragging) return
-    isDragging = false;
-    autoSlide();
-}
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+  if (!isDragging) return;
+  isDragging = false;
+  autoSlide();
+};
 
-
-
-
-// window.onload = function () {
-//     var elements = document.querySelectorAll('*');
-//     elements.forEach(function (element) {
-//         element.classList.add('enable-transition');
-//     });
-// };
-
-
-carousel.addEventListener('touchmove', dragging);
-carousel.addEventListener('touchstart', dragStart);
-carousel.addEventListener('touchend', dragStop);
-carousel.addEventListener('touchcancel', dragStop);
-carousel.addEventListener('mousemove', dragging);
-carousel.addEventListener('mousedown', dragStart);
-carousel.addEventListener('mouseup', dragStop);
-carousel.addEventListener('mouseleave', dragStop);
+carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("touchend", dragStop);
+carousel.addEventListener("touchcancel", dragStop);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
 
 setTimeout(widthCalculate, 500);
