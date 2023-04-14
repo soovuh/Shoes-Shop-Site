91ec50671cd0bfde27bcec643f3e13c0e9307647
Klymenko Illia <sovuh2703@gmail.com>
2023-04-14 15:40:28 +0300

    Adding autoloading to hot deals

--- src/controllers/index-control.js
+++ src/controllers/index-control.js
@@ -1,5 +1,5 @@
 // Simulating a request
-import { carouselObjs } from "../database.js";
+import { carouselObjs, hotDeals } from "../database.js";
 
 // Add images from database.js to carousel
 function fillCarousel(carousel) {
@@ -17,4 +17,60 @@ function fillCarousel(carousel) {
   });
 }
 
-export { fillCarousel };
+function fillHotDeals(container) {
+  // loop through each product in the array
+  for (let i = 0; i < hotDeals.length; i++) {
+    const product = hotDeals[i];
+
+    // create the card element
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    // create the image element and add it to the card
+    const imageLink = document.createElement("a");
+    imageLink.href = "#";
+    const image = document.createElement("img");
+    image.classList.add("card-image");
+    image.src = product.image;
+    image.alt = "";
+    imageLink.appendChild(image);
+    card.appendChild(imageLink);
+
+    // create the product name, old price, and current price elements and add them to the card
+    const productLink = document.createElement("a");
+    productLink.href = "#";
+    productLink.classList.add("product-a");
+    const content = document.createElement("div");
+    content.classList.add("card-content");
+    const productName = document.createElement("p");
+    productName.classList.add("product-name");
+    productName.textContent = product.name;
+    const oldPrice = document.createElement("p");
+    oldPrice.classList.add("old-price");
+    oldPrice.textContent = product.price + " $";
+    const currentPrice = document.createElement("p");
+    currentPrice.classList.add("current-price");
+    currentPrice.textContent =
+      Math.round(product.price * (1 - product.sale)) + " $";
+    content.appendChild(productName);
+    content.appendChild(oldPrice);
+    content.appendChild(currentPrice);
+    productLink.appendChild(content);
+    card.appendChild(productLink);
+
+    // create the "view more" link and add it to the card
+    const infoLink = document.createElement("a");
+    infoLink.href = "#";
+    infoLink.classList.add("product-a");
+    const info = document.createElement("div");
+    info.classList.add("card-info");
+    info.textContent = "View more";
+    infoLink.appendChild(info);
+    card.appendChild(infoLink);
+
+    // add the card to the container
+    container.appendChild(card);
+  }
+}
+
+export { fillCarousel, fillHotDeals };
--- src/start/index-start.js
+++ src/start/index-start.js
@@ -1,9 +1,11 @@
-import { fillCarousel } from "../controllers/index-control.js";
+import { fillCarousel, fillHotDeals } from "../controllers/index-control.js";
 import { indexAnimation } from "../animation/index-script.js";
 
+const cardContainer = document.querySelector(".cards-container");
 const carousel = document.querySelector(".carousel");
 const carouselImg = document.querySelector(".carousel-imgs");
 const arrowIcons = document.querySelectorAll(".carousel .icon");
 
+fillHotDeals(cardContainer);
 fillCarousel(carouselImg);
 indexAnimation(carousel, arrowIcons);
