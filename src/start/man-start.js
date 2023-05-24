7b803f9c3e55155b2df3351db7dcc9134e10a40a
Klymenko Illia <sovuh2703@gmail.com>
2023-05-24 14:42:10 +0300

    Created search logic, rework searchinputs, and categories start

--- src/start/man-start.js
+++ src/start/man-start.js
@@ -12,15 +12,32 @@ async function getObjs(url) {
 }
 
 async function filterShoesBySex() {
-  const shoesObjs = await getObjs("http://127.0.0.1:8000/shoe/");
-  const brandObjs = await getObjs("http://127.0.0.1:8000/brand/");
+  const urlParams = new URLSearchParams(window.location.search);
+  const search = urlParams.get("search");
+  if (search) {
+    const shoesObjsSearch = await getObjs(
+      `http://127.0.0.1:8000/shoe/?search=${search}`
+    );
+    const brandObjsSearch = await getObjs("http://127.0.0.1:8000/brand/");
+    const manObjectsSearch = shoesObjsSearch.filter(
+      (obj) => obj.sex === "male"
+    );
+    const womanbtn = document.querySelector(".woman");
+    const searchResult = document.querySelector(".result_search");
+    const manbtn = document.querySelector(".man");
+    searchResult.innerHTML = search;
+    womanbtn.href = `woman.html?search=${search}`;
+    manbtn.href = `man.html?search=${search}`;
 
-  // Filtering shoes objects by sex
-
-  const manObjects = shoesObjs.filter((obj) => obj.sex === "male");
-
-  startFiltering(manObjects, brandObjs);
-  categoriesAnimation();
+    startFiltering(manObjectsSearch, brandObjsSearch);
+    categoriesAnimation();
+  } else {
+    const shoesObjs = await getObjs(`http://127.0.0.1:8000/shoe/`);
+    const brandObjs = await getObjs("http://127.0.0.1:8000/brand/");
+    const manObjects = shoesObjs.filter((obj) => obj.sex === "male");
+    startFiltering(manObjects, brandObjs);
+    categoriesAnimation();
+  }
 }
 
 await filterShoesBySex();
