96247f12e3bdfa37c3bf09d725235818eea7fa41
Klymenko Illia <sovuh2703@gmail.com>
2023-06-06 19:21:50 +0300

    Created data & settings block, added inputs and style, created fetch request for user info

--- src/controllers/profile-control.js
+++ src/controllers/profile-control.js
@@ -1,8 +1,31 @@
 import { baseLink } from "../constants.js";
 import { getCookie } from "./authentication_check.js";
 
-function profileControlStart() {
+function profileControlStart(userInfo) {
+  const inputFields = document.querySelectorAll(".input-box input");
   const logoutBtn = document.querySelector(".logout");
+  const labels = document.querySelectorAll(".input-box label");
+  const changeBtn = document.querySelector(".change");
+  const saveBtn = document.querySelector(".save-btn");
+  const usernameInput = document.querySelector("#username");
+  const phoneNumberInput = document.querySelector("#phone");
+  const addressInput = document.querySelector("#address");
+
+  usernameInput.value = userInfo.username;
+  if (userInfo.phone_number) {
+    phoneNumberInput.value = userInfo.phone_number;
+  }
+  if (userInfo.address) {
+    addressInput.value = userInfo.address;
+  }
+
+  changeBtn.addEventListener("click", () => {
+    saveBtn.classList.add("active");
+    inputFields.forEach((field) => {
+      field.removeAttribute("disabled");
+      field.classList.add("active");
+    });
+  });
 
   logoutBtn.addEventListener("click", () => {
     document.cookie =
@@ -10,6 +33,19 @@ function profileControlStart() {
     document.cookie =
       "sessionid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = "../../public/pages/login.html";
+  });
+
+  inputFields.forEach((inputField, index) => {
+    if (inputField.value) {
+      labels[index].style.top = "-5px";
+    }
+    inputField.addEventListener("input", () => {
+      if (inputField.value === "") {
+        labels[index].style.removeProperty("top");
+      } else {
+        labels[index].style.top = "-5px";
+      }
+    });
   });
 }
 
--- src/start/profile-start.js
+++ src/start/profile-start.js
@@ -1,5 +1,26 @@
 import { profileControlStart } from "../controllers/profile-control.js";
+import { baseLink } from "../constants.js";
+import { getCookie } from "../controllers/authentication_check.js";
 
 document.querySelector("#loader").style.display = "none";
 
-profileControlStart();
+async function getuserInfo() {
+  const csrfToken = getCookie("csrftoken");
+  const sessionId = getCookie("sessionid");
+  const resp = await fetch(`${baseLink}/accounts/get_user_info/`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "X-CSRFToken": csrfToken,
+      Cookie: `sessionid=${sessionId}`,
+    },
+    credentials: "include",
+  });
+
+  const data = await resp.json();
+  return data;
+}
+
+const userInfo = await getuserInfo();
+
+profileControlStart(userInfo);
