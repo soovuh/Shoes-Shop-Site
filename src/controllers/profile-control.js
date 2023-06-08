f12a53f77986febe4eff295b1653eff5abf3dbd1
Klymenko Illia <sovuh2703@gmail.com>
2023-06-08 13:39:21 +0300

    Create prepareing obj before request, and rework address

--- src/controllers/profile-control.js
+++ src/controllers/profile-control.js
@@ -10,16 +10,27 @@ function profileControlStart(userInfo) {
   const usernameInput = document.querySelector("#username");
   const phoneNumberInput = document.querySelector("#phone");
   const addressInput = document.querySelector("#address");
+  const cityInput = document.querySelector("#city");
+  const streetInput = document.querySelector("#street");
+  const postcodeInput = document.querySelector("#postcode");
   const addressData = document.querySelectorAll(".address-data");
   const addressBox = document.querySelector(".address");
   const moveLabels = document.querySelectorAll(".move");
 
   usernameInput.value = userInfo.username;
+  const addressObj = userInfo.address ? userInfo.address : {};
   if (userInfo.phone_number) {
     phoneNumberInput.value = userInfo.phone_number;
   }
-  if (userInfo.address) {
-    addressInput.value = userInfo.address;
+  if (addressObj.city) {
+    addressInput.value = `${addressObj.city} city, ${addressObj.street} street, postcode: ${addressObj.postcode}`;
+    cityInput.value = addressObj.city;
+    streetInput.value = addressObj.street;
+    postcodeInput.value = addressObj.postcode;
+  } else {
+    addressObj.city = "";
+    addressObj.street = "";
+    addressObj.postcode = "";
   }
 
   changeBtn.addEventListener("click", () => {
@@ -43,6 +54,44 @@ function profileControlStart(userInfo) {
     addressBox.setAttribute("disabled", "disabled");
     addressBox.classList.remove("active");
     addressBox.classList.add("off");
+  });
+
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const username = usernameInput.value;
+    const phoneNumber = phoneNumberInput.value;
+    const city = cityInput.value;
+    const street = streetInput.value;
+    const postcode = postcodeInput.value;
+    const requestObj = {};
+    if (!postcode && !city && !street) {
+      if (username != userInfo.username) {
+        requestObj.username = username;
+      }
+      if (phoneNumber != userInfo.phoneNumber) {
+        requestObj.phone_number = phoneNumber;
+      }
+      console.log(requestObj);
+    } else if (postcode && address && street) {
+      if (username != userInfo.username) {
+        requestObj.username = username;
+      }
+      if (phoneNumber != userInfo.phoneNumber) {
+        requestObj.phone_number = phoneNumber;
+      }
+      if (city != addressObj.city) {
+        requestObj.city = city;
+      }
+      if (street != addressObj.street) {
+        requestObj.street = street;
+      }
+      if (postcode != addressObj.postcode) {
+        requestObj.postcode = postcode;
+      }
+      console.log(requestObj);
+    } else {
+      console.log("error!");
+    }
   });
 
   logoutBtn.addEventListener("click", () => {
